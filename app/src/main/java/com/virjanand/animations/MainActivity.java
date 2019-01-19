9cfe29732c5b56679803be2c1d7c9172cc77b962
Virjanand <virjanand@virjanand.com>
2019-01-19 06:01:36 +0100

    Added homer image and change alpha in animation to fade bart out and homer in and vice versa

--- app/src/main/java/com/virjanand/animations/MainActivity.java
+++ app/src/main/java/com/virjanand/animations/MainActivity.java
@@ -4,16 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isBartVisible;
+
     public void fade(View view) {
-        Log.i("info", "click");
+
+        float alphaBart = 1;
+
+        if (isBartVisible) {
+            alphaBart = 0;
+            isBartVisible = false;
+        } else {
+            isBartVisible = true;
+        }
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+        bartImageView.animate().alpha(alphaBart).setDuration(2000);
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+        homerImageView.animate().alpha(1-alphaBart).setDuration(2000);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isBartVisible = true;
     }
 }
