161df652c9021fe5dafd52052962c3d7a94fba4c
Virjanand <virjanand@virjanand.com>
2019-01-19 05:44:45 +0100

    Renamed image files to all lowercase and not using -

--- app/src/main/java/com/virjanand/animations/MainActivity.java
+++ app/src/main/java/com/virjanand/animations/MainActivity.java
@@ -2,8 +2,14 @@ package com.virjanand.animations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+        Log.i("info", "click");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
